966488269cc8a2358272c38994d9dd3d70ca4f03
megarbry <megarbry@gmail.com>
2020-05-28 16:13:41 -0600

    Started creating project

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 $(document).ready(function() {
-   $('#input-name').on('keyup', function() {
-       let name = $(this).val();
-      $('#feedback-message').text('Nice to meet you ' + name);
-   })
-
-   $('a').on('click', function(e) {
-        e.preventDefault();
-        $('#feedback-message').text('That\'s fine');
-   })
+   
 });
