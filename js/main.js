1af8ab9d01ab3584135536539c87f93b9bfa658a
megarbry <megarbry@gmail.com>
2020-05-28 15:54:34 -0600

    Difference between keydown and keyup event listeners

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 $(document).ready(function() {
-   $('#select-menu').on('change', function(){
-       let name = $('#select-menu option:selected').text();
-       let dist = $('#select-menu option:selected').val();
-       let price = $('#select-menu option:selected').data('price');
-
-       if(dist) {
-            $('#feedback-message').text('You are signing up for a ' + name + ', which costs ' + price + ', to a distance of ' + dist + 'km');
-       } else {
-           $('#feedback-message').text('');
-       }
-
-       
+   $('#input-name').on('keyup', function() {
+       let name = $(this).val();
+      $('#feedback-message').text('Nice to meet you ' + name);
    })
 });
