4e73991dfc2af2ad8516721002373c423ae73a95
megarbry <megarbry@gmail.com>
2020-05-28 16:26:21 -0600

    Learned how to add html items and append/prepend methods

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-   
+   $('#button-create-item').on('click', function() {
+       let name = $('#input-create-item').val();
+       $('#input-create-item').val('');
+
+       let html = '';
+       html += '<div class="item">';
+            html += '<div class="name">' + name + '</div>';
+            html += '<img src="assests/origamiFlower.jpg" alt="Tokyo pic">';
+            html += '<div class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>';
+            html += '<div class="price">499</div>';
+            html += '<button>Add to cart</button>';
+            html += '<br/>';
+            html += '<a href="#">More info</a>';
+            html += '<div class="more-info">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</div>';
+        html += '</div>';
+
+        $('#container').append(html);
+   })
 });
